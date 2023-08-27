7f8702a22ac0c88e62a788a95ad5bf8bfb681391
Harsh Muriki <48922732+harshmuriki@users.noreply.github.com>
2023-08-27 02:54:43 +0000

    Added basic backend code:

---
+++ main.py
@@ -0,0 +1,18 @@
+# import backend.model as model
+from backend.model import LlamaThread
+
+file = open("test-data/harsh-resume-raw.txt", mode="r")
+
+raw_data = file.read()
+
+job_desc = open("test-data/job-description-1.txt", mode="r")
+
+job_description = job_desc.read()
+
+context = f'"This is my current resume: {raw_data}. Edit the resume based on this jobs description {job_description} and give me a suitable resume"'
+
+model = LlamaThread("llama-2-7b-chat")
+model.start()
+output = model.ask_question("", context)
+
+print(output)
