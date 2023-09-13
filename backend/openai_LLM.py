1fcd2b4d8970489e4eac456f7e4eb68340e2a297
harshmuriki <harshsuhith@gmail.com>
2023-09-13 03:35:28 -0400

    Added code that gets the job description directly from the website and converts the new edited resume to a latex file based on jake's resume format

--- backend/openai_LLM.py
+++ backend/openai_LLM.py
@@ -1,7 +1,7 @@
 import openai
 
 
-def prompt(init_prompt, question):
+def openai_prompt(init_prompt, question):
     openai.api_key = "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"
 
     # response = openai.ChatCompletion.create(
@@ -23,7 +23,12 @@ def prompt(init_prompt, question):
         ]
     )
 
-    print()
+    # response = openai.Completion.create(
+    #     engine="text-embedding-ada-002",  # You can choose the appropriate engine
+    #     prompt=question,
+    #     max_tokens=16385  # Adjust this number to control the length of the response
+    # )
+    # generated_text = response.choices[0].text
 
     # arr = response["choices"][0]["text"]
 
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-# import backend.model as model
-from backend.model import LlamaThread
-
-file = open("test-data/harsh-resume-raw.txt", mode="r")
-
-raw_data = file.read()
-
-job_desc = open("test-data/job-description-1.txt", mode="r")
-
-job_description = job_desc.read()
-
-context = f'"This is my current resume: {raw_data}. EDIT the resume based on this jobs description {job_description} and give me a suitable resume"'
-
-question = f'"This is my resume {raw_data}. \n\n I want you to act as an ai resume editor giving me just the edited, newer version of my resume based on this job description: {job_description}. \n\n"'
-# print(question)
+import backend.pdfparser as openaiLLM
 
 
-question = f'"\nThis is my resume {raw_data}. \n\n Job description: {job_description}."'
-model = LlamaThread("llama-2-7b-chat")
-model.start()
-output = model.ask_question("", question)
+resumePath = "test-data/Resume_Harsh_Muriki_SWE.pdf"
+jobDesPath = "test-data/job-description-1.txt"
 
-print(output)
+job_link = "https://www.linkedin.com/jobs/collections/recommended/?currentJobId=3729882071"
+website_data = openaiLLM.get_JobData(job_link)  # scraping
+
+jobDescription = openaiLLM.get_JobDescription(
+    job_link)  # gets the job description only
+
+print("jobDescription", jobDescription)
+
+edited_resume = openaiLLM.edit_complete_resume(resumePath, jobDescription)
+
+print(edited_resume)
+
+formated_latex = openaiLLM.convert_to_latex(edited_resume)
+
+# print(formated_latex)
+with open('formatexLatex.txt', 'w') as f:
+    f.write(formated_latex)
